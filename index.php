2178912b8705edb5ed67b40c2b5b3601c3b7d095
Richardo2003 <s22110407@student.unklab.ac.id>
2024-11-21 23:40:27 +0800

    adding new project

--- index.php
+++ index.php
@@ -1,32 +1,5 @@
 <?php
-// Autoload untuk semua kelas
-spl_autoload_register(function ($class) {
-    require_once './app/core/' . $class . '.php';
-});
 
-// Koneksi database
-$database = new Core\Database();
-$db = $database->getConnection();
+require_once './app/core/Database.php';
 
-// Include controller dan model yang dibutuhkan
-require_once './app/controllers/PengeluaranController.php';
-require_once './app/models/Pengeluaran.php';
-
-// Inisialisasi controller dengan model
-$model = new Pengeluaran($db);
-$controller = new PengeluaranController($model);
-
-// Routing berdasarkan parameter URL
-$url = isset($_GET['url']) ? rtrim($_GET['url'], '/') : 'list';
-
-// Menangani request berdasarkan URL
-if ($url == 'list') {
-    $controller->index(); // Menampilkan daftar pengeluaran
-} elseif ($url == 'add' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    $controller->add($_POST); // Menambahkan pengeluaran baru
-} elseif (strpos($url, 'delete/') === 0) {
-    $id = explode('/', $url)[1]; // Mendapatkan ID dari URL
-    $controller->delete($id); // Menghapus pengeluaran
-} else {
-    echo "Halaman tidak ditemukan.";
-}
+include './app/view/listPengeluaran.php';
