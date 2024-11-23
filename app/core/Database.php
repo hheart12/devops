fd5f0e62bfae484c012c8dc0b66a17160ff6fef9
Richardo2003 <s22110407@student.unklab.ac.id>
2024-11-23 13:17:23 +0800

    fix database

--- app/core/Database.php
+++ app/core/Database.php
@@ -8,13 +8,9 @@ use PDOException;
 define("APP_PATH", "http://103.59.95.145/unkpresent/devops/");
 
 class Database {
-    private static $instance = null;
-    private static $connection;
-
-    // Constructor private untuk mencegah instansiasi langsung
-    private function __construct() {
+    // Konstruktor menjadi publik
+    public function __construct() {
         try {
-            // Koneksi ke database menggunakan PDO
             self::$connection = new PDO(
                 'mysql:host=localhost;dbname=db_catatpengeluaran', 
                 'testadmin', 
@@ -22,16 +18,8 @@ class Database {
             );
             self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            // Tangani exception jika koneksi gagal
             die("Connection failed: " . $e->getMessage());
         }
     }
-
-    // Ubah getConnection menjadi metode statis
-    public static function getConnection() {
-        if (self::$instance === null) {
-            self::$instance = new Database();
-        }
-        return self::$connection;
-    }
 }
+
